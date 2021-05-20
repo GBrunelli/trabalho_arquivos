2ca0735c5c155cb582ba46ae04daf728e83c1fa6
GBrunelli <gubrunelli@hotmail.com>
2021-05-20 14:42:27 -0300

    Sketch of CarsCsvToBin()

--- src/routines.c
+++ src/routines.c
@@ -1,2 +1,20 @@
 #include "car.h"
 #include "line.h"
+
+void CarsCsvToBin(FILE* file, char* binFileName)
+{
+    // open the binary file
+    FILE* bin = fopen(binFileName, "r+b");
+    
+    // read the header from the csv
+    CarHeader* carHeader = getCarHeader(file, CSV);
+
+    // while there are cars in the csv
+
+        // read the car from the csv
+        // write the car in the bin
+
+    // write the header in the bin
+
+    // free the memory
+}
