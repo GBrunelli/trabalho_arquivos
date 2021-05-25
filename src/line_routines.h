7a042135b9397dcc390f9badb016e3368615a3b9
João Guilherme Jarochinski Marinho <pato@MacBook-Pro-de-Joao.local>
2021-05-24 21:26:25 -0300

    Decomposing routines file into 2 files. One for cars/vehicles and other for lines

---
+++ src/line_routines.h
@@ -0,0 +1,20 @@
+#ifndef LINE_ROUTINES_H
+#define LINE_ROUTINES_H
+
+#include "line.h"
+
+/* ## LINE ROUTINES ###*/
+
+// 2th task -> INSERT ALL
+void LinesCsvToBin();
+
+// 4nd task -> SELECT ALL
+void printAllLines();
+
+// 6rd task -> SELECT WHERE Campo = Valor
+void printSelectedLines();
+
+// 8th task -> INSERT N REGISTERS INTO END
+void InsertNewLinesBin();
+
+#endif
--- src/routines.h
+++ src/routines.h
@@ -1,7 +1,6 @@
 #ifndef ROUTINES_H
 #define ROUTINES_H
 
-#include "line.h"
 #include "car.h"
 
 /* ## CAR ROUTINES ## */
@@ -18,19 +17,5 @@ void printSelectedCars();
 // 7th task -> INSERT N REGISTERS INTO END
 void InsertNewCarsBin();
 
-
-/* ## LINE ROUTINES ###*/
-
-// 2th task -> INSERT ALL
-void LinesCsvToBin();
-
-// 4nd task -> SELECT ALL
-void printAllLines();
-
-// 6rd task -> SELECT WHERE Campo = Valor
-void printSelectedLines();
-
-// 8th task -> INSERT N REGISTERS INTO END
-void InsertNewLinesBin();
 
 #endif
