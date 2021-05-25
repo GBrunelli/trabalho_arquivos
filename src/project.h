421a628f545ebc5ebc0903ee830d045a53cb6c6e
João Guilherme Jarochinski Marinho <joaogui3856@hotmail.com>
2021-05-25 11:43:09 -0300

    Refactoring what should go in project or utils

--- src/project.h
+++ src/project.h
@@ -7,10 +7,4 @@
 #include <stdbool.h>
 #include <string.h>
 
-// Maximum field size for each struct.
-#define MAX_STRING_SIZE 100
-
-// Possible file sources.
-typedef enum _Source { CSV, CLI, BIN } Source;
-
 #endif
--- src/utils.h
+++ src/utils.h
@@ -3,6 +3,18 @@
 
 #include "project.h"
 
+// Message used when printing fields that are currently Null
+#define NULL_MESSAGE = "campo com valor nulo"
+
+// Maximum field size for each struct.
+#define MAX_STRING_SIZE 100
+
+// Possible file sources.
+typedef enum _Source { CSV, CLI, BIN } Source;
+
+typedef enum _DataType { CAR, LINE } DataType;
+
+
 void leftShift(char *string, int len);
 int isNULO(char* string);
 void fillWithGarbage(char* string, int len);
