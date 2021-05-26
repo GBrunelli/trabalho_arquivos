21b78b6375e37785975d3fc76b8d6b84bdfcda70
GBrunelli <gubrunelli@hotmail.com>
2021-05-26 14:15:12 -0300

    Implemented PrintAllCars

--- src/utils.h
+++ src/utils.h
@@ -4,7 +4,7 @@
 #include "project.h"
 
 // Message used when printing fields that are currently Null
-#define NULL_MESSAGE = "campo com valor nulo"
+#define NULL_MESSAGE "campo com valor nulo"
 
 // Maximum field size for each struct.
 #define MAX_STRING_SIZE 100
