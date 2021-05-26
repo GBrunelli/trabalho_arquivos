60bb9c09b9b7ecc7484af5d6cb41e10ee08c3851
João Guilherme Jarochinski Marinho <joaogui3856@hotmail.com>
2021-05-26 13:42:07 -0300

    Commenting routine

--- src/line_routines.c
+++ src/line_routines.c
@@ -2,12 +2,15 @@
 #include "line.h"
 
 void LinesCsvToBin() {
+    // Getting CSV and BIN filenames
     char csvFileName[MAX_STRING_SIZE], binFileName[MAX_STRING_SIZE];
     scanf("%s %s", csvFileName, binFileName);
 
     // Opening both src and dest files
     FILE* csvFile = fopen(csvFileName, "r");
     FILE* binFile = fopen(binFileName, "wb+");
+
+    // Error handling didn't open
     if (csvFile == NULL || binFile == NULL) {
         printf("Falha no processamento do arquivo.\n");
         fclose(csvFile);
@@ -25,17 +28,19 @@ void LinesCsvToBin() {
     // Setting file as INCONSISTENT
     setLineFileStatus(binFile, STATUS_INCONSISTENT);
     
-    // Instantiating a car holder
+    // Instantiating a Line and then writing all CSV Lines into BIN file
     Line* l = newLine();
-    while(updateLine(l, csvFile, CSV) == OK) // while there are cars in the csv
+    while(updateLine(l, csvFile, CSV) == OK) 
     {
-        writeLine(l, binFile, BIN); // write the car in the bin 
+        writeLine(l, binFile, BIN);  
     }
+
+    // Finishing program. Freeing all memory
     freeLine(l);
     fclose(csvFile);
-    
     setLineFileStatus(binFile, STATUS_CONSISTENT);
     fclose(binFile);
     
+    // Proof that our program worked correctly
     binarioNaTela(binFileName);
 }
